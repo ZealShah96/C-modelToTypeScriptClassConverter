23e0f817a796bfa5c6812ae7c071a4e29564e6e2
zealshah96 <Zealshah96@gmail.com>
2018-09-12 17:56:45 +0530

    adding multiple folder capabilities

--- fileRead.js
+++ fileRead.js
@@ -4,30 +4,27 @@ var filesNames=[];
 
 var _dirName="";
 var writefilePath="";
-var allowedkeyWordsString=['#region','#endregion','public','class','string','int'];
+var allowedkeyWordsString=['#region','#endregion','public','class','string','int','bool','DateTime'];
 
 file.readFile('./config.json',(err,data)=>{
     let jsonOfFilesConfig=JSON.parse(data);
-    _dirName=jsonOfFilesConfig.ModelFolders;
-     writefilePath=jsonOfFilesConfig.pastingFolder;
-     processStart();
+    var dirnames=jsonOfFilesConfig.ModelFolders;
+    Object.entries(dirnames).forEach(([key,value])=>{
+        _dirName=value;
+        writefilePath=jsonOfFilesConfig.pastingFolder;
+        processStart(_dirName,writefilePath);
+    });
+    
 });
 
-
-
-
-// filesNames.forEach(element => {
-   
-// });
-
-function processStart(){
+function processStart(_dirName,writefilePath){
     file.readdir(_dirName, (err, files) => {
         files.forEach(fileName => {
           console.log(fileName);
           filesNames.push(fileName);
           file.readFile(_dirName+fileName,{encoding: 'utf-8'},function(err,data){
             if(!err){
-            findLocationsOfbrackets(data);
+            findLocationsOfbrackets(data,writefilePath);
             }
             else{
             console.log(err);
@@ -41,7 +38,7 @@ function processStart(){
 
 
 
-function findLocationsOfbrackets(data){
+function findLocationsOfbrackets(data,writefilePath){
 var trimdata=replaceSpecificCharcter('\n',replaceSpecificCharcter('\r',data.trim(),''),'');
 //console.log(trimdata);
 let arrayOfSentence=trimdata.split('{').join('').split('}').join('').split(' ');
@@ -57,7 +54,7 @@ if(element!="" && element!="get;" && element!="set;" && index>firstClassOccuranc
 });
 console.log(arrayOfSentence);
 // iterateThroughArrayOfStatement();
-prepareClassStringandElemenetsString(arrayOfSentence);
+prepareClassStringandElemenetsString(arrayOfSentence,writefilePath);
 }
 
 
@@ -83,7 +80,7 @@ function iterateThroughArrayOfStatement(className,classPrepareStringfromfunction
 
 
 
-function prepareClassStringandElemenetsString(arr){
+function prepareClassStringandElemenetsString(arr,writefilePath){
     var jsonOfClassStructure={};
     var className="";
     var tempArry=[];
@@ -98,12 +95,26 @@ function prepareClassStringandElemenetsString(arr){
             }
             else{
                 if(allowedkeyWordsString.indexOf(arr[i+1])>-1)
-                stringofclasselement+=arr[i]+" "+arr[i+2]+":"+arr[i+1].replace('int','number')+", ";
+                stringofclasselement+=arr[i]+" "+makeFirstCharSmall(arr[i+2])+":"+arr[i+1].replace('int','number').replace('DateTime','Date').replace('bool','boolean')+", ";
+                // var arrayofstringofclasselement=stringofclasselement.split('');
+                // arrayofstringofclasselement[0]=arrayofstringofclasselement[0].toLowerCase();
+                // stringofclasselement=arrayofstringofclasselement.join('').toString();
+                
             }
             i=i+2;
         }
     }
   var k=iterateThroughArrayOfStatement(className,stringofclasselement);
     file.appendFileSync(writefilePath, "\n"+iterateThroughArrayOfStatement(className,stringofclasselement));
+}
+
+
+function makeFirstCharSmall(string){
+
+    var tempArry=string.split('');
+    console.log(tempArry["0"]);
+    tempArry["0"]=tempArry["0"].toLowerCase();
+
+    return tempArry.join('');
 }
 
